5520a466e4ac6226e5a0e6c09d8abc46074a1b19
admoredigital20 <admoredigital20@gmail.com>
2021-10-02 13:26:53 +0530

    updated1

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ function sliderClick(event) {
 }
 
 function slider1() {
-    document.getElementById("bsns-content").innerHTML = "Influencer management"
+    document.getElementById("bsns-content").innerHTML = "Influencer management";
+    document.getElementById("bsns-img").style.backgroundImage = "url('./public/assets/img/bsnconsult-img2.png')";
 }
 
 function slider2() {
     document.getElementById("bsns-content").innerHTML = "Business education"
+    document.getElementById("bsns-img").style.backgroundImage = "url('./public/assets/img/bsnconsult-img3.png')";
 }
 
 function slider3() {
     document.getElementById("bsns-content").innerHTML = "Marketing  consulting"
+    document.getElementById("bsns-img").style.backgroundImage = "url('./public/assets/img/bsnconsult-img4.png')";
 }
 
 function slider4() {
     document.getElementById("bsns-content").innerHTML = "Business consulting"
+    document.getElementById("bsns-img").style.backgroundImage = "url('./public/assets/img/bsnconsult-img.png')";
 }
